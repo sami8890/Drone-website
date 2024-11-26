e0c1fccae05b70f9062cc7836664b250e07a0ad3
sami8890 <samigabol12@gmail.com>
2024-11-26 19:26:44 +0500

    updated code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,16 @@
 import Hero from "@/components/ui/Hero-Section";
 import ProductsSection from "./Product/page";
 import TestimonialSection from "@/components/ui/testimonial";
-// import Footer from "./footer/footer";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function Home() {
   return (
     <section>
-     
-      <Hero />
-      <ProductsSection/>
-      <TestimonialSection/>
- 
+      <Analytics /> 
 
+      <Hero />
+      <ProductsSection />
+      <TestimonialSection />
     </section>
   );
 }
